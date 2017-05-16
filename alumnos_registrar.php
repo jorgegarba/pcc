0ace8a75ba4f8d82730f6aaaf6142fa9390a61fb
Jorge Garnica Blanco <jorgegarba@gmail.com>
2017-05-16 15:07:34 -0500

    Formulario de alumnos V2
    Formulario de registro de alumnos casi terminado! WIN!

--- alumnos_registrar.php
+++ alumnos_registrar.php
@@ -18,7 +18,51 @@
                     <label for="nom_per">Nombre:</label>
                     <input class="form-control" placeholder="Escriba su Nombre:" required="required" name="nom_per" id="nom_per">
                 </fieldset>
-
+                <fieldset class="form-group">
+                    <label for="ape_per">Apellido:</label>
+                    <input class="form-control" placeholder="Escriba su Apellido:" required="required" name="ape_per" id="ape_per">
+                </fieldset>
+                <fieldset class="form-group">
+                    <label for="dni_per">DNI:</label>
+                    <input class="form-control" placeholder="Escriba su DNI:" required="required" name="dni_per" id="dni_per" maxlength="8">
+                </fieldset>
+                <fieldset class="form-group">
+                    <label for="fec_nac_per">Fecha de Nacimiento:</label>
+                    <input type="date" class="form-control" required="required" name="fec_nac_per" id="fec_nac_per">
+                </fieldset>
+                <fieldset class="form-group">
+                    <label>Sexo:</label>
+                    <div class="radio">
+                        <label>
+                            <input type="radio" name="sex_per" id="sex_per1" value="H" checked> Hombre
+                        </label>
+                    </div>
+                    <div class="radio">
+                        <label>
+                            <input type="radio" name="sex_per" id="sex_per2" value="M"> Mujer
+                        </label>
+                    </div>
+                </fieldset>
+                <fieldset class="form-group">
+                    <label for="email_per">Correo Electrónico:</label>
+                    <input type="email" class="form-control" placeholder="Escriba su Correo Electrónico:" required="required" name="email_per" id="email_per" maxlength="50">
+                </fieldset>
+                <fieldset class="form-group">
+                    <label for="cel_per">Número de Celular:</label>
+                    <input class="form-control" placeholder="Escriba su Número de Celular" required="required" name="cel_per" id="cel_per" maxlength="15">
+                </fieldset>
+                <fieldset class="form-group">
+                    <label for="dir_per">Dirección:</label>
+                    <input class="form-control" placeholder="Escriba su Dirección" required="required" name="dir_per" id="dir_per" maxlength="100">
+                </fieldset>
+                <fieldset class="form-group">
+                    <label for="log_per">Usuario:</label>
+                    <input class="form-control" placeholder="Cree un usuario para acceder al sistema" required="required" name="log_per" id="log_per" maxlength="100">
+                </fieldset>
+                <fieldset class="form-group">
+                    <label for="pass_per">Contraseña:</label>
+                    <input type="password" class="form-control" placeholder="Cree una contraseña para acceder al sistema" required="required" name="pass_per" id="pass_per" maxlength="100">
+                </fieldset>
                 <button type="submit" class="btn btn-secondary">Submit Button</button>
                 <button type="reset" class="btn btn-secondary">Reset Button</button>
             </form>
