3532a12cd727c529b28f7fd653c9c379a45aef4e
Jorge Garnica Blanco <jorgegarba@gmail.com>
2017-05-16 12:02:06 -0500

    Subiendo plantilla maestra
    Creando una carpeta para guardar la plantilla máistra... NO MODIFICAR
    LOS ARCHIVOS DENTRO DE ESTA CARPETA

--- documentacion/script_inicialV1.sql
+++ documentacion/script_inicialV1.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
 --
--- Host: localhost    Database: pfr
+-- Host: localhost    Database: pcc
 -- ------------------------------------------------------
 -- Server version	5.7.11-log
 
